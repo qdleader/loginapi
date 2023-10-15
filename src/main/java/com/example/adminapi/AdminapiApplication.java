8b2aba03cb7589899ebcf864f46800fa77ee760b
前端架构师 <62106726+qdleader@users.noreply.github.com>
2023-10-15 21:02:24 +0800

    feat:后台接口添加

---
+++ src/main/java/com/example/adminapi/AdminapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.adminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adminapi/AdminapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
